d0dda36ba415af6c87af2449524a5ad6817a0baf
aksuharun <harun16aksu@gmail.com>
2023-10-17 23:57:00 +0300

    Initial commit

---
+++ index.js
@@ -0,0 +1,9 @@
+const charCount = 16
+const chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789 ~!@#$%^&*()_-+={[}]|\:;\<,>.?/"
+let password = '';
+
+for(let i = 0; i < charCount; i++) {
+	password += chars.charAt(Math.floor(Math.random() * chars.length)); 
+}
+
+console.log(password);
